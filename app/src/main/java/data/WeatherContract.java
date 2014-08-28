78f2a306ea0a483af34e80844055ec7d2606d9fd
Changming Fan <ChangmingFan@Gmail.com>
2014-08-28 14:14:10 -0400

    22th Commit   OK - lesson 3   data direcotory: 2 classess 1. . WeatherContract 2. WeatherDBHelperAdd: 082814 220AM

---
+++ app/src/main/java/data/WeatherContract.java
@@ -0,0 +1,7 @@
+package data;
+
+/**
+ * Created by Changming on 8/28/2014.
+ */
+public class WeatherContract {
+}
---
+++ app/src/main/java/data/WeatherDBHelper.java
@@ -0,0 +1,9 @@
+package data;
+
+
+/**
+ * Created by Changming on 8/26/2014.
+ */
+public class WeatherDBHelper {
+    
+}
