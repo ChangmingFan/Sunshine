7daa34b2ede02ae54ff27aedf04a5e09a13269c7
ChangnmingFan <ChangnmingFan@Gmail.com>
2014-08-28 21:06:00 -0400

    29th commit 082814 9PM

--- app/src/main/java/data/WeatherContract.java
+++ app/src/main/java/data/WeatherContract.java
@@ -1,7 +1,9 @@
 package data;
 
 /**
- * Created by Changming on 8/28/2014. Adding push 23th commit 
+ * Created by Changming on 8/28/2014. 29th commit  9PM
+ * Created by Changming on 8/28/2014. Adding push 23th commit
+ *
  */
 public class WeatherContract {
 }
