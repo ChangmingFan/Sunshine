d1f95bd66d9f052ec41c478888ce148a75f27421
ChangnmingFan <ChangnmingFan@Gmail.com>
2014-08-28 21:15:20 -0400

    30th commit almost same as 29th  914pm

--- app/src/main/java/data/WeatherContract.java
+++ app/src/main/java/data/WeatherContract.java
@@ -1,7 +1,7 @@
 package data;
 
 /**
- * Created by Changming on 8/28/2014. 29th commit  9PM
+ * Created by Changming on 8/28/2014. 30th commit  914PM
  * Created by Changming on 8/28/2014. Adding push 23th commit
  *
  */
